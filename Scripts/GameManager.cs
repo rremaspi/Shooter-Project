6f68b30b25e152c841ce96a7904b2cdc874ef795
rremaspi <rremaspi@gmail.com>
2024-11-02 18:30:10 -0400

    Original Scripts

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player, transform.position, Quaternion.identity);
+        InvokeRepeating("CreateEnemy", 1f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateEnemy()
+    {
+        Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 9f, 0), Quaternion.identity);
+    }
+}
